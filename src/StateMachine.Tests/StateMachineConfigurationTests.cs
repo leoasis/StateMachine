e9afb3d03c09fc1887cdeb768a2b3e56e13232a3
Leonardo Garcia Crespo <leoasis@gmail.com>
2011-09-02 13:09:51 -0300

    Started api concept

---
+++ src/StateMachine.Tests/StateMachineConfigurationTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+namespace StateMachine.Tests
+{
+    [TestFixture]
+    public class StateMachineConfigurationTests
+    {
+        [Test]
+        public void api_concept()
+        {
+            
+        }
+
+
+        private class Vehicle
+        {
+            private bool _seatbeltOn;
+
+            public Vehicle()
+            {
+                State = StateMachine<VehicleStates>.For<Vehicle>.Create(cfg =>
+                {
+                    cfg.Initial = VehicleStates.Parked;
+                    cfg.BeforeTransition
+                        .From(VehicleStates.Parked).To(States<VehicleStates>.Any - VehicleStates.Parked)
+                        .Do(PutOnSeatBelt);
+
+                    cfg.AfterTransition.On("Crash").Do(Tow);
+                    cfg.AfterTransition.On("Repair").Do(Fix);
+                    cfg.AfterTransition
+                        .From(States<VehicleStates>.Any).To(VehicleStates.Parked)
+                        .Do(vehicle => vehicle._seatbeltOn = false);
+                });
+            }
+
+            private void PutOnSeatBelt()
+            {
+                _seatbeltOn = true;
+            }
+
+            private void Tow()
+            {
+
+            }
+
+            private void Fix()
+            {
+
+            }
+
+            public StateMachine<VehicleStates> State { get; private set; }
+        }
+
+        private enum VehicleStates
+        {
+            Parked,
+            Idling,
+            FirstGear,
+            SecondGear
+        }
+    }
+}
---
+++ src/StateMachine/StateMachine.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace StateMachine
+{
+    public class StateMachine<TStateType>
+    {
+        public static class For<TEntity>
+        {
+            public static StateMachine<TStateType> Create(Action<IStateMachineConfiguration<TStateType, TEntity>> configuration)
+            {
+                return new StateMachine<TStateType>();
+            }
+        }
+    }
+
+    public interface IStateMachineConfiguration<TStateType, TEntity>
+    {
+        TStateType Initial { set; }
+        ITransitionHookBuilder<TStateType, TEntity> BeforeTransition { get; set; }
+        ITransitionHookBuilder<TStateType, TEntity> AfterTransition { get; set; }
+    }
+
+    public interface ITransitionHookBuilder<TStateType, TEntity>
+    {
+        ITransitionHookFrom<TStateType, TEntity> From(params TStateType[] states);
+        ITransitionHookDo<TStateType, TEntity> On(string eventName);
+    }
+
+    public interface ITransitionHookFrom<TStateType, TEntity>
+    {
+        ITransitionHookDo<TStateType, TEntity> To(params TStateType[] states);
+    }
+
+    public interface ITransitionHookDo<TStateType, TEntity>
+    {
+        void Do(Action action);
+        void Do(Action<TEntity> action);
+    }
+   
+
+    public class States<T>
+    {
+        private static States<T> _any = new States<T>();
+        private readonly T[] _states;
+
+
+        public static States<T> Any { get { return _any; } }
+
+        public static T[] operator -(States<T> state, T value)
+        {
+            return new[] { value };
+        }
+
+        public static implicit operator T[](States<T> state)
+        {
+            return state._states;
+        }
+
+        public States(params T[] states)
+        {
+            if (states.Length == 0)
+                _states = Enum.GetValues(typeof (T)).Cast<T>().ToArray();
+            _states = states;
+        }
+    }
+}
